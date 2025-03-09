b7699d5660927175ece1253e01b48990206402b0
Lucifer <38715155+jonesroot@users.noreply.github.com>
2025-03-09 23:02:08 +0700

    update sshws/insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -5,8 +5,8 @@ cd
 apt install python -y
 
 #Install Script Websocket-SSH Python
-wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/rendyap100/sshmax-premium/main/sshws/dropbear-ws2.py
-wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/rendyap100/sshmax-premium/main/sshws/ws-stunnel2
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/jonesroot/sshmax-premium/main/sshws/dropbear-ws2.py
+wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/jonesroot/sshmax-premium/main/sshws/ws-stunnel2
 
 #izin permision
 chmod +x /usr/local/bin/ws-dropbear
@@ -14,10 +14,10 @@ chmod +x /usr/local/bin/ws-stunnel
 
 
 #System Dropbear Websocket-SSH Python
-wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/rendyap100/sshmax-premium/main/sshws/service-wsdropbear2 && chmod +x /etc/systemd/system/ws-dropbear.service
+wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/jonesroot/sshmax-premium/main/sshws/service-wsdropbear2 && chmod +x /etc/systemd/system/ws-dropbear.service
 
 #System SSL/TLS Websocket-SSH Python
-wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/rendyap100/sshmax-premium/main/sshws/ws-stunnel2.service && chmod +x /etc/systemd/system/ws-stunnel.service
+wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/jonesroot/sshmax-premium/main/sshws/ws-stunnel2.service && chmod +x /etc/systemd/system/ws-stunnel.service
 
 #restart service
 #
