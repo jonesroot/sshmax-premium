03452ddf9c8531cd0621a945dfb0cd97706876cf
Lucifer <38715155+jonesroot@users.noreply.github.com>
2025-03-09 23:03:18 +0700

    update limit3/quota.sh

--- limit3/quota.sh
+++ limit3/quota.sh
@@ -1,4 +1,4 @@
-REPO="https://raw.githubusercontent.com/rendyap100/sshmax-premium/main/"
+REPO="https://raw.githubusercontent.com/jonesroot/sshmax-premium/main/"
 wget -q -O /etc/systemd/system/limitvmess.service "${REPO}limit3/limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limitvless.service "${REPO}limit3/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limittrojan.service "${REPO}limit3/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
@@ -7,11 +7,11 @@ wget -q -O /etc/xray/limit.vmess "${REPO}limit3/vmess" >/dev/null 2>&1
 wget -q -O /etc/xray/limit.vless "${REPO}limit3/vless" >/dev/null 2>&1
 wget -q -O /etc/xray/limit.trojan "${REPO}limit3/trojan" >/dev/null 2>&1
 wget -q -O /etc/xray/limit.shadowsocks "${REPO}limit3/shadowsocks" >/dev/null 2>&1
-#wget https://raw.githubusercontent.com/rendyap100/sshmax-premium/main/limi3/ip-shadowsocks
-#wget https://raw.githubusercontent.com/rendyap100/sshmax-premium/main/limi3/ip-trojan
-#wget https://raw.githubusercontent.com/rendyap100/sshmax-premium/main/limi3/ip-vless
-#wget https://raw.githubusercontent.com/rendyap100/sshmax-premium/main/limi3/ip-vmess
-#wget https://raw.githubusercontent.com/rendyap100/sshmax-premium/main/limi3/limit
+#wget https://raw.githubusercontent.com/jonesroot/sshmax-premium/main/limi3/ip-shadowsocks
+#wget https://raw.githubusercontent.com/jonesroot/sshmax-premium/main/limi3/ip-trojan
+#wget https://raw.githubusercontent.com/jonesroot/sshmax-premium/main/limi3/ip-vless
+#wget https://raw.githubusercontent.com/jonesroot/sshmax-premium/main/limi3/ip-vmess
+#wget https://raw.githubusercontent.com/jonesroot/sshmax-premium/main/limi3/limit
 chmod +x /etc/xray/limit.vmess
 chmod +x /etc/xray/limit.vless
 chmod +x /etc/xray/limit.trojan
